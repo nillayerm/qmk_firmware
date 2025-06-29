2b5fcb96f18bfffbb331c514e5ee8542054dffbc
nillayerm <smnam123@naver.com>
2025-06-29 10:18:01 +0900

    Fix macro keycodes and added LT to space
    Added macro function 'QM_TGED', to be used for 'end' key hold. Updated the base and FN2 layer keymaps to use LT(_FN2, KC_SPC) and adjusted tapping term for this key.

--- keyboards/ymdk/id75/keymaps/nillayerm/keymap.c
+++ keyboards/ymdk/id75/keymaps/nillayerm/keymap.c
@@ -28,7 +28,7 @@ enum layer_names {
 enum custom_macros {
     // gaming macros
     QM_TGFW = SAFE_RANGE, // hold down 'w' to continuously moving forward in game
-    QM_TGML //hold down 'end' to toggle melee attack
+    QM_TGED,              // hold down 'end' to toggle melee attack
 };
 
 // Tap Dance keycodes
@@ -43,7 +43,7 @@ enum td_keycodes {
     MN_SL, // minus and slash
     K9_LB, // o and left bracket
     K0_RB, // p and right bracket
-    EN_BS  // enter and backslash
+    EN_BS, // enter and backslash
 };
 
 // various actions for Tap Dance
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Base Layer (Default Layer) */
     [_BASE] = LAYOUT_ortho_5x15(
-        QK_GESC,           KC_1,    KC_2,    KC_3,    KC_4,   KC_5,     TD(BS_ES), TD(PL_AS), TD(MN_SL),        KC_6,    KC_7,   KC_8,    TD(K9_LB), TD(K0_RB), KC_PSCR,
-        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,     KC_7,      KC_8,      KC_9,             KC_Y,    KC_U,   KC_I,    KC_O,      KC_P,      KC_BSPC,
-        KC_LSFT,           KC_A,    KC_S,    KC_D,    KC_F,   KC_G,     KC_4,      KC_5,      KC_6,             KC_H,    KC_J,   KC_K,    KC_L,      KC_RSFT,   KC_ENT,
-        LT(_FN2, KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,     KC_1,      KC_2,      KC_3,             KC_N,    KC_M,   KC_COMM, KC_DOT,    KC_UP,     KC_SLSH,
-        KC_LCTL,           KC_LGUI, KC_LALT, KC_SCLN, KC_SPC, MO(_FN1), TD(EN_BS), KC_0,      LT(_FN2, KC_DOT), KC_QUOT, KC_SPC, KC_RALT, KC_LEFT,   KC_DOWN,   KC_RGHT
+        QK_GESC,           KC_1,    KC_2,    KC_3,      KC_4,               KC_5,     TD(BS_ES), TD(PL_AS), TD(MN_SL),        KC_6,    KC_7,   KC_8,    TD(K9_LB), TD(K0_RB), KC_PSCR,
+        KC_TAB,            KC_Q,    KC_W,    KC_E,      KC_R,               KC_T,     KC_7,      KC_8,      KC_9,             KC_Y,    KC_U,   KC_I,    KC_O,      KC_P,      KC_BSPC,
+        KC_LSFT,           KC_A,    KC_S,    KC_D,      KC_F,               KC_G,     KC_4,      KC_5,      KC_6,             KC_H,    KC_J,   KC_K,    KC_L,      KC_RSFT,   KC_ENT,
+        LT(_FN2, KC_CAPS), KC_Z,    KC_X,    KC_C,      KC_V,               KC_B,     KC_1,      KC_2,      KC_3,             KC_N,    KC_M,   KC_COMM, KC_DOT,    KC_UP,     KC_SLSH,
+        KC_LCTL,           KC_LGUI, KC_LALT, KC_SCLN,   LT(_FN2, KC_SPC),   MO(_FN1), TD(EN_BS), KC_0,      LT(_FN2, KC_DOT), KC_QUOT, KC_SPC, KC_RALT, KC_LEFT,   KC_DOWN,   KC_RGHT
     ),
 
     /* FN1 Layer */
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* FN2 Layer */
     [_FN2] = LAYOUT_ortho_5x15(
         _______, _______, _______, _______, _______, _______, KC_F11,  KC_EQL, KC_F12,  _______,   _______,   _______, _______,  _______, _______,
-        KC_MPRV, _______, QM_TGFW, QM_TGML, _______, _______, KC_F7,   KC_F8,  KC_F9,   _______,   _______,   _______, _______,  KC_INS,  KC_DEL,
+        KC_MPRV, _______, QM_TGFW, QM_TGED, _______, _______, KC_F7,   KC_F8,  KC_F9,   _______,   _______,   _______, _______,  _______, _______,
         KC_MNXT, _______, _______, _______, KC_PGDN, KC_PGUP, KC_F4,   KC_F5,  KC_F6,   _______,   _______,   _______, _______,  _______, _______,
         KC_TRNS, KC_END,  _______, _______, _______, _______, KC_F1,   KC_F2,  KC_F3,   _______,   TD(MU_PL), _______, _______,  _______, _______,
         KC_MPLY, _______, _______, _______, _______, _______, _______, KC_F10, KC_TRNS, TD(VD_PR), TD(VU_NX), _______, _______,  _______, _______
@@ -294,17 +294,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         // customized macros
+        case QM_TGED: // keep running
+            if (record->event.pressed) {
+                SEND_STRING(
+                    SS_DOWN(X_END)
+                );
+            }
+            break;
         case QM_TGFW: // keep running
             if (record->event.pressed) {
                 SEND_STRING(
                     SS_DOWN(X_W)
-                );
-            }
-            break;
-        case QM_TGML: // keep meleeing
-            if (record->event.pressed) {
-                SEND_STRING(
-                    SS_DOWN(X_END)
                 );
             }
             break;
@@ -315,6 +315,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // tapping term adjustment here
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        case LT(_FN2, KC_SPC):
+            return 120;
         case LT(_FN2, KC_CAPS):
             return 155;
         case LT(_FN2, KC_DOT):
