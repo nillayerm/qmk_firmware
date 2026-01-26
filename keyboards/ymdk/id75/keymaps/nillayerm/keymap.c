4e3f81596b5f1496247d6fc10d2bb3c352f20e39
nillayerm <smnam123@naver.com>
2026-01-26 16:33:54 +0900

    Update keymap.c

--- keyboards/ymdk/id75/keymaps/nillayerm/keymap.c
+++ keyboards/ymdk/id75/keymaps/nillayerm/keymap.c
@@ -38,12 +38,12 @@ enum td_keycodes {
     MU_PL, // 'mute' and 'play'
     VD_PR, // 'volume down' and 'previous track'
     VU_NX, // 'volume up' and 'next track'
-    NL_EC, // 'numlock' and 'escape'
+    BK_NL, // 'backspace' and 'numlock'
     PL_EQ, // 'plus' and 'asterisk'
     MN_SL, // 'minus' and 'slash'
     PD_PA, // 'numpad dot' and 'numpad asterisk'
     KM_CO, // 'm' and 'comma'
-    BK_ET, // 'backspace' and 'enter'
+    ET_EC, // 'enter' and 'escape'
     N9_LB, // '9' and 'left bracket'
     N0_RB, // '0' and 'right bracket'
     PS_BS, // 'Print Screen' and 'backslash'
@@ -90,31 +90,30 @@ void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
         .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}),               \
     }
 
-const key_override_t backspace_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_MINS);
-const key_override_t pscr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_PSCR, KC_UNDS);
+const key_override_t backspace_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_PPLS);
+
 
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
     &backspace_key_override,
-    &pscr_key_override,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Base Layer (Default Layer) */
     [_BASE] = LAYOUT_ortho_5x15(
-        QK_GESC,  KC_1,     KC_2,     KC_3,               KC_4,    KC_5,      TD(NL_EC),  TD(PL_EQ),  TD(MN_SL),  KC_6,    KC_7,    KC_8,      TD(N9_LB),  TD(N0_RB),  TD(PS_BS),
+        QK_GESC,  KC_1,     KC_2,     KC_3,               KC_4,    KC_5,      TD(BK_NL),  TD(PL_EQ),  TD(MN_SL),  KC_6,    KC_7,    KC_8,      TD(N9_LB),  TD(N0_RB),  TD(PS_BS),
         KC_TAB,   KC_Q,     KC_W,     KC_E,               KC_R,    KC_T,      KC_P7,      KC_P8,      KC_P9,      KC_Y,    KC_U,    KC_I,      KC_O,       KC_P,       KC_BSPC,
         KC_LSFT,  KC_A,     KC_S,     KC_D,               KC_F,    KC_G,      KC_P4,      KC_P5,      KC_P6,      KC_H,    KC_J,    KC_K,      KC_L,       KC_RSFT,    XXXXXXX,
         XXXXXXX,  KC_SLSH,  KC_Z,     KC_X,               KC_C,    KC_V,      KC_P1,      KC_P2,      KC_P3,      KC_B,    KC_N,    TD(KM_CO), KC_DOT,     KC_UP,      KC_ENT,
-        KC_LCTL,  KC_LGUI,  KC_LALT,  LT(_FN2, KC_SCLN),  KC_SPC,  MO(_FN1),  TD(BK_ET),  KC_P0,      TD(PD_PA),  KC_QUOT, KC_SPC,  KC_RALT,   KC_LEFT,    KC_DOWN,    KC_RGHT
+        KC_LCTL,  KC_LGUI,  KC_LALT,  LT(_FN2, KC_SCLN),  KC_SPC,  MO(_FN1),  TD(ET_EC),  KC_P0,      TD(PD_PA),  KC_QUOT, KC_SPC,  KC_RALT,   KC_LEFT,    KC_DOWN,    KC_RGHT
     ),
 
     /* FN1 Layer */
     [_FN1] = LAYOUT_ortho_5x15(
         KC_F12,    KC_F1,      KC_F2,     KC_F3,    KC_F4,    KC_F5,      _______,    _______,    _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,      KC_F10,   KC_F11,
-        KC_CAPS,   C(KC_Q),    C(KC_W),   C(KC_E),  C(KC_R),  C(KC_T),    _______,    _______,    _______,  _______,  _______,  _______,  _______,    _______,  RM_TOGG,
-        KC_ENT,    C(KC_A),    C(KC_S),   C(KC_D),  C(KC_F),  MO(_SYST),  _______,    _______,    _______,  _______,  _______,  _______,  TG(_LOCK),  _______,  XXXXXXX,
+        _______,   C(KC_Q),    C(KC_W),   C(KC_E),  C(KC_R),  C(KC_T),    _______,    _______,    _______,  _______,  _______,  _______,  _______,    _______,  RM_TOGG,
+        KC_CAPS,   C(KC_A),    C(KC_S),   C(KC_D),  C(KC_F),  MO(_SYST),  _______,    _______,    _______,  _______,  _______,  _______,  TG(_LOCK),  _______,  XXXXXXX,
         XXXXXXX,   C(S(KC_T)), C(KC_Z),   C(KC_X),  C(KC_C),  C(KC_V),    TD(VU_NX),  _______,    _______,  _______,  _______,  _______,  _______,    RM_VALU,  _______,
         _______,   _______,    _______,   _______,  _______,  KC_TRNS,    TD(VD_PR),  TD(MU_PL),  _______,  _______,  _______,  KC_RCTL,  RM_PREV,    RM_VALD,  RM_NEXT
     ),
@@ -122,8 +121,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* FN2 Layer */
     [_FN2] = LAYOUT_ortho_5x15(
         _______, _______, _______, _______, _______, _______, _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,    _______,
-        KC_CAPS, _______, QM_TGFW, QM_TGED, _______, _______, _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,    _______,
-        KC_ENT,  _______, _______, _______, _______, _______, _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,    XXXXXXX,
+        _______, _______, QM_TGFW, QM_TGED, _______, _______, _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,    _______,
+        KC_CAPS, _______, _______, _______, _______, _______, _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,    XXXXXXX,
         XXXXXXX, _______, _______, QK_LOCK, _______, _______, TD(VU_NX),  _______,    _______,  _______,  _______,  _______,  _______,  TD(VU_NX),  _______,
         _______, _______, _______, KC_TRNS, _______, _______, TD(VD_PR),  TD(MU_PL),  _______,  _______,  _______,  _______,  _______,  TD(VD_PR),  TD(MU_PL)
     ),
@@ -172,12 +171,12 @@ tap_dance_action_t tap_dance_actions[] = {
     [MU_PL] = ACTION_TAP_DANCE_TAP_HOLD(KC_MUTE, KC_MPLY),
     [VD_PR] = ACTION_TAP_DANCE_TAP_HOLD(KC_VOLD, KC_MPRV),
     [VU_NX] = ACTION_TAP_DANCE_TAP_HOLD(KC_VOLU, KC_MNXT),
-    [NL_EC] = ACTION_TAP_DANCE_TAP_HOLD(KC_NUM, KC_ESC),
+    [BK_NL] = ACTION_TAP_DANCE_TAP_HOLD(KC_BSPC, KC_NUM),
     [PL_EQ] = ACTION_TAP_DANCE_TAP_HOLD(KC_PPLS, KC_EQL),
     [MN_SL] = ACTION_TAP_DANCE_TAP_HOLD(KC_MINS, KC_SLSH),
     [PD_PA] = ACTION_TAP_DANCE_TAP_HOLD(KC_PDOT, KC_PAST),
     [KM_CO] = ACTION_TAP_DANCE_TAP_HOLD(KC_M, KC_COMM),
-    [BK_ET] = ACTION_TAP_DANCE_TAP_HOLD(KC_BSPC, KC_ENT),
+    [ET_EC] = ACTION_TAP_DANCE_TAP_HOLD(KC_ENT, KC_ESC),
     [N9_LB] = ACTION_TAP_DANCE_TAP_HOLD(KC_9, KC_LBRC),
     [N0_RB] = ACTION_TAP_DANCE_TAP_HOLD(KC_0, KC_RBRC),
     [PS_BS] = ACTION_TAP_DANCE_TAP_HOLD(KC_PSCR, KC_BSLS),
@@ -218,7 +217,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             tap_code16(tap_hold->tap);
             }
             break;
-        case TD(NL_EC):
+        case TD(BK_NL):
             action = tap_dance_get(QK_TAP_DANCE_GET_INDEX(keycode));
             state = tap_dance_get_state(QK_TAP_DANCE_GET_INDEX(keycode));
             if (!record->event.pressed && state != NULL && state->count && !state->finished) {
@@ -258,7 +257,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             tap_code16(tap_hold->tap);
             }
             break;
-        case TD(BK_ET):
+        case TD(ET_EC):
             action = tap_dance_get(QK_TAP_DANCE_GET_INDEX(keycode));
             state = tap_dance_get_state(QK_TAP_DANCE_GET_INDEX(keycode));
             if (!record->event.pressed && state != NULL && state->count && !state->finished) {
